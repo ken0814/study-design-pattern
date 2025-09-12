1f24b7a4e5ecb89545ebb5fba0a9ccb3dd50efdf
ken.lee <ken19990814@gmail.com>
2025-09-12 18:10:33 +0800

    新增 prototype partten 範例

---
+++ creational-patterns/go/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+)
+
+type User struct {
+	Name  string
+	Age   int
+	Email string
+}
+
+// 淺拷貝
+func (u *User) Clone() *User {
+	return &User{
+		Name:  u.Name,
+		Age:   u.Age,
+		Email: u.Email,
+	}
+}
+
+// 深拷貝
+func (u *User) DeepClone() *User {
+	clone := *u
+	return &clone
+}
+
+func main() {
+	fmt.Println("Prototype Pattern 原形模式")
+	original := &User{Name: "ken", Age: 26, Email: "ken@example.com"}
+	clone := original.Clone()
+	clone.Name = "joyce"
+	clone.Email = "joyce@example.com"
+	fmt.Printf("Original point:%p\nOriginal value: %+v\n", original, original)
+	fmt.Printf("Clone point:%p\nClone value: %+v\n", clone, clone)
+}
